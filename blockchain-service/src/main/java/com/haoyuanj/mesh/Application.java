6b8c46bdd16757938ca95ea06b648c46473f40d1
HaoYuan Jiang <jhypewpew@gmail.com>
2025-06-10 13:26:52 +0800

    Set up logging

--- blockchain-service/src/main/java/com/haoyuanj/mesh/Application.java
+++ blockchain-service/src/main/java/com/haoyuanj/mesh/Application.java
@@ -1,7 +1,7 @@
 package com.haoyuanj.mesh;
 
-import java.util.Arrays;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
+  Logger logger = LoggerFactory.getLogger(Application.class);
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
@@ -18,13 +20,7 @@ public class Application {
   @Bean
   public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
     return args -> {
-      System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-      String[] beanNames = ctx.getBeanDefinitionNames();
-      Arrays.sort(beanNames);
-      for (String beanName : beanNames) {
-        System.out.println(beanName);
-      }
+      logger.info("Application started.");
     };
   }
 }
--- blockchain-service/src/main/java/com/haoyuanj/mesh/HelloWorldController.java
+++ blockchain-service/src/main/java/com/haoyuanj/mesh/HelloWorldController.java
@@ -1,13 +1,18 @@
 package com.haoyuanj.mesh;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+  Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
   @GetMapping("/")
   public String index() {
+    logger.info("Greetings endpoint hit");
     return "Greetings from Spring Boot!";
   }
 }
