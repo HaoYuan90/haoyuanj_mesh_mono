64dcfab68d2119455590e2de03e6b6e9e04ac6f3
HaoYuan Jiang <jhypewpew@gmail.com>
2025-06-19 16:07:24 +0800

    Update chat server

--- chat-service/lib/socket_handler.ex
+++ chat-service/lib/socket_handler.ex
@@ -2,6 +2,22 @@ defmodule ChatService.SocketHandler do
   require Logger
   @behaviour :cowboy_websocket
 
+  def message_id(msg_payload) do
+    cond do
+      !Map.has_key?(msg_payload, "data") ->
+        nil
+
+      !Map.has_key?(msg_payload["data"], "message") ->
+        nil
+
+      !Map.has_key?(msg_payload["data"]["message"], "message_id") ->
+        nil
+
+      true ->
+        msg_payload["data"]["message"]["message_id"]
+    end
+  end
+
   def init(request, _state) do
     state = %{registry_key: request.path}
 
@@ -24,23 +40,23 @@ defmodule ChatService.SocketHandler do
              )},
           any()
         ) :: {:reply, {:text, any()}, any()}
-  def websocket_handle({:text, json}, state) do
-    Logger.info("Received message: #{json}")
+  def websocket_handle({:text, json_payload}, state) do
+    Logger.info("Received message: #{json_payload}")
 
     try do
-      payload = Jason.decode!(json)
-      message = payload["data"]["message"]
+      payload = Jason.decode!(json_payload)
+      message_id = message_id(payload)
 
       Registry.ChatService
-      |> Registry.dispatch(state.registry_key, fn(entries) ->
+      |> Registry.dispatch(state.registry_key, fn entries ->
         for {pid, _} <- entries do
           if pid != self() do
-            Process.send(pid, message, [])
+            Process.send(pid, json_payload, [])
           end
         end
       end)
 
-      {:reply, {:text, message}, state}
+      {:reply, {:text, message_id}, state}
     rescue
       _e in Jason.DecodeError ->
         Logger.error("Failed to decode message")
